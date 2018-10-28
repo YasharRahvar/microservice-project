d09ec213a3feef4c22d4c7c5f7064f9c1cc1c056
Yashar Rahvar <Yashar.Rahvar@buzzmove.com>
2018-10-28 17:08:35 +0000

    Adding proxy service with zull

---
+++ proxy-service/src/main/java/ca/yrhv/proxy/ProxyServiceApplication.java
@@ -0,0 +1,43 @@
+package ca.yrhv.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ProxyServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProxyServiceApplication.class, args);
+    }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
--- simple/src/main/java/ca/yrhv/simple/controller/SimpleController.java
+++ simple/src/main/java/ca/yrhv/simple/controller/SimpleController.java
@@ -1,10 +1,10 @@
-package ca.yrhv.registry.controller;
+package ca.yrhv.simple.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class simpleController {
+@RestController(value = "/simple")
+public class SimpleController {
 
     @GetMapping(value = "/hello")
     public String hello() {
